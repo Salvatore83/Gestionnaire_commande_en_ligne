a9365ef3646f8ce2034b6be7e5b0b456ed6e7576
Hamanite83 <47501835+Hamanite83@users.noreply.github.com>
2019-02-10 19:45:27 +0100

    Maj Fenêtre
    Fenêtre créée

--- programme_commande/GUI/gui.py
+++ programme_commande/GUI/gui.py
@@ -1,3 +1,48 @@
+from . import PythonLja_18 as Lja
+
+    
+#######################################################################
+#
+#                   creer_fenetre()
+#
+#    paramètres : largeur, hauteur
+#
+#         do : crée la fenêtre de largeur et de hauteur définie
+#         return : Rien
+#
+#######################################################################
+
+
+def f_creer_fenetre(largeur,hauteur):
+
+    #
+    # Crée la fenêtre
+    #
+
+    Lja.init_window("Projet", largeur, hauteur)
+
+    #
+    # Fait durer la fenêtre sous windows
+    #
+
+    Lja.main_loop()
+
+
+#######################################################################
+#
+#                   ouvrir_gestionnaire()
+#
+#    paramètres : 
+#
+#         do : ouvre le gestionnaire via l'interface graphique
+#         return : Rien
+#
+#######################################################################
 
 def f_ouvrir_gestionnaire():
-    print('Ouvertur du gestionnaire via GUI')
+    print('Ouverture du gestionnaire via GUI')
+    largeur = int(input("Saisissez la largeur de votre fenêtre: "))
+    print('\n')
+    hauteur = int(input("Saisissez la largeur de votre fenêtre: "))
+    f_creer_fenetre(largeur,hauteur)
+
