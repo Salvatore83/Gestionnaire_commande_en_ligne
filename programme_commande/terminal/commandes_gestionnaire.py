e58d9d670a54909e8aaf8721e10b37cb397f8b9e
Hamanite83 <47501835+Hamanite83@users.noreply.github.com>
2019-02-15 21:47:38 +0100

    GUI DE FOLIE
    REVEILLE EN TOI LE TOURBILLION D'UN GUI DE FOLIIIIIIIIIIIE

--- programme_commande/terminal/commandes_gestionnaire.py
+++ programme_commande/terminal/commandes_gestionnaire.py
@@ -7,11 +7,24 @@ def f_gerer_commande_utilisateur():
     valide = 0
 
     while valide != True:
+        #
+        # Demande a l'utilisateur ce qu'il veut que le robot fasse
+        #
         print('Que voulez vous faire ?')
-        print(' >')
-        commande = input('')
+        commande = input('> ')
 
+        #
+        # Gestion de toutes les conditions des commandes AJOUTER PLUS TARD
+        #
+        #
+        # Si l'utilisateur veut faire avancer le robot
+        #
         if commande == 'avancer':
-            pass
+            valide = True
+        #
+        # Si l'utilisateur veut quitter
+        #
+        if commande == 'quitter':
+            valide = True
 
-"""Il faut que je gere les commandes la du coup"""
+    return commande
--- programme_commande/terminal/connexion.py
+++ programme_commande/terminal/connexion.py
@@ -28,11 +28,16 @@ import socket
 
 def f_creer_serveur():
 
+    #
+    # Creation du socket serveur
+    #
     socket_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     host = ''
     port = 5566
-
+    #
+    # Ajout de l'adresse et du port au socket serveur
+    #
     socket_serveur.bind((host, port))
 
     return socket_serveur
@@ -55,9 +60,17 @@ def f_creer_serveur():
 
 def f_accepter_connexion(para_socket_serveur):
 
+    #
+    # Ecoute des connexions sur l'adresse via le port
+    #
     para_socket_serveur.listen(3)
+    #
+    # Accepte la connexion et retourne un nouveau socket, le socket client ainsi que l'adresse du client
+    #
     socket_client, adresse = para_socket_serveur.accept()
-
+    #
+    # Affiche un message pour savoir qui vient de se connecter
+    #
     print('Une connexion a eut lieu {}'.format(adresse[0]))
 
     return socket_client
@@ -77,9 +90,44 @@ def f_accepter_connexion(para_socket_serveur):
 ###########################################################################################################################################
 
 def f_envoyer_message(para_socket_client, para_message):
-
-    message = message.encode('utf8')
+    #
+    # Encode le message a envoyer
+    #
+    message = para_message.encode('utf8')
+    #
+    # Envoie le message par le socket
+    #
     para_socket_client.send(message)
+
+
+
+###########################################################################################################################################
+#
+#           f_attendre_robot_pres()
+#
+#       para : socket client
+#
+#       do :
+#           attend un message
+#
+#       return : message
+#
+###########################################################################################################################################
+
+def f_recevoir_message(para_socket_client):
+    #
+    # Attend un message via la connexion et le sauvegarde dans la variable message
+    #
+    message = para_socket_client.recv(1024)
+    #
+    # Decode le message reçut (utf8)
+    #
+    message = message.decode('utf8')
+
+    return message
+
+
+
 
 ###########################################################################################################################################
 #
@@ -97,14 +145,75 @@ def f_envoyer_message(para_socket_client, para_message):
 def f_attendre_robot_pret(para_socket_serveur):
 
     message = '0'
-
-    while message != '1':
-
+    #
+    # Tant que le robot n'est pas pret
+    #
+    while message != 'Robot pret':
+        #
+        # Attendre message et le decoder
+        #
         message = para_socket_serveur.recv(1024)
         message = message.decode('utf8')
 
     print('robot pret')
 
 
-def fermer_socket(para_socket):
+
+###########################################################################################################################################
+#
+#           f_gerer_commande()
+#
+#       para : socket client et commande
+#
+#       do :
+#           envoie une commande au robot et attend qu'il execute l'action
+#
+#       return : rien
+#
+###########################################################################################################################################
+
+
+def f_gerer_commande(para_socket_serveur, para_socket_client, para_commande):
+
+    #
+    # Envoie la commande au socket client (robot)
+    #
+    f_envoyer_message(para_socket_client, para_commande)
+
+    message = ''
+    #
+    # Tant que le robot n'a pas reçut le message
+    #
+    while message != 'Bien reçut':
+        message = f_recevoir_message(para_socket_client)
+
+    print('Le robot a bien reçut l action')
+    message = ''
+    #
+    # Tant que le robot n'a pas finit l'action
+    #
+    while message != 'Action terminee':
+        message = f_recevoir_message(para_socket_client)
+        print('Le robot execute l action ...')
+
+    print('Le robot a finit l action')
+
+###########################################################################################################################################
+#
+#           f_fermer_socket()
+#
+#       para : socket
+#
+#       do :
+#           ferme le socket
+#
+#       return : rien
+#
+###########################################################################################################################################
+
+
+def f_fermer_socket(para_socket):
+    #
+    # Ferme le socket passe en parametre
+    #
     para_socket.close()
