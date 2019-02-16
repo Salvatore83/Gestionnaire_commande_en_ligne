b207c151660a3140ef953a5dabf62363df7dad17
Hamanite83 <47501835+Hamanite83@users.noreply.github.com>
2019-02-16 15:54:06 +0100

    MAJ GUI

--- programme_commande/GUI/actions.py
+++ programme_commande/GUI/actions.py
@@ -16,6 +16,7 @@ from . import interface as logo
 ###########################################################################################################################################
 
 def f_RecupCoord():
+    PositionClic = 0
     #
     # Récupère la postion de la souris sur les axes x et y
     #
@@ -24,7 +25,7 @@ def f_RecupCoord():
     #
     # Si la souris est dans le carré de gauche:
     #
-    if sourisx > 10 and sourisx < 110 and sourisy > 150 and sourisy < 250:
+    if sourisx > 10 and sourisx < 110 and sourisy > 150 and sourisy < 250 and PositionClic == 0:
         #
         # Affiche l'allure cliquée du bouton de gauche
         #
@@ -35,10 +36,11 @@ def f_RecupCoord():
         logo.f_BoutonAvant()
         logo.f_BoutonDroite()
         logo.f_BoutonArriere()
+        PositionClic = 1
         #
         # Sinon, si la souris est dans le carré du haut:
         #
-    elif sourisx > 115 and sourisx < 215 and sourisy > 45 and sourisy < 145:
+    elif sourisx > 115 and sourisx < 215 and sourisy > 45 and sourisy < 145 and PositionClic == 0:
         #
         # Affiche l'allure cliquée du bouton du haut
         #
@@ -49,10 +51,11 @@ def f_RecupCoord():
         logo.f_BoutonGauche()
         logo.f_BoutonDroite()
         logo.f_BoutonArriere()
+        PositionClic = 2
         #
         # Sinon, si la souris est dans le carré de droite:
         #
-    elif sourisx > 220 and sourisx < 320 and sourisy > 150 and sourisy < 250:
+    elif sourisx > 220 and sourisx < 320 and sourisy > 150 and sourisy < 250 and PositionClic == 0:
         #
         # Affiche l'allure cliquée du bouton de droite
         #
@@ -63,8 +66,9 @@ def f_RecupCoord():
         logo.f_BoutonGauche()
         logo.f_BoutonAvant()
         logo.f_BoutonArriere()
+        PositionClic = 3
         # Sinon, si la souris est dans le carré du bas:
-    elif sourisx > 115 and sourisx < 215 and sourisy > 255 and sourisy < 355:
+    elif sourisx > 115 and sourisx < 215 and sourisy > 255 and sourisy < 355 and PositionClic == 0:
         #
         # Affiche l'allure cliquée du bouton du bas
         #
@@ -75,6 +79,48 @@ def f_RecupCoord():
         logo.f_BoutonGauche()
         logo.f_BoutonDroite()
         logo.f_BoutonAvant()
+        PositionClic = 4
+
+        ###############################################################################################################################
+        # ATTENTION: A PARTIR D'ICI, C'EST POUR FAIRE EN SORTE QUE LES BOUTONS SOIENT EUX-MEMES "DECLIQUABLES"; NE MARCHE PAS ENCORE. #
+        ###############################################################################################################################
+
+        #
+        # Si on est sur le carré de gauche et qu'il a déjà été cliqué:
+        #
+    elif sourisx > 10 and sourisx < 110 and sourisy > 150 and sourisy < 250 and PositionClic == 1:
+        #
+        # Rétablit l'apparence basique du bouton
+        # 
+        PositionClic = 0
+        logo.f_BoutonGauche()
+        #
+        # Si on est sur le carré du haut et qu'il a déjà été cliqué:
+        #
+    elif sourisx > 115 and sourisx < 215 and sourisy > 45 and sourisy < 145 and PositionClic == 2:
+        #
+        # Rétablit l'apparence basique du bouton
+        # 
+        PositionClic = 0
+        logo.f_BoutonAvant()
+        #
+        # Si on est sur le carré de droite et qu'il a déjà été cliqué:
+        #
+    elif sourisx > 220 and sourisx < 320 and sourisy > 150 and sourisy < 250 and PositionClic == 3:
+        #
+        # Rétablit l'apparence basique du bouton
+        # 
+        PositionClic = 0
+        logo.f_BoutonDroite()
+        #
+        # Si on est sur le carré du bas et qu'il a déjà été cliqué:
+        #
+    elif sourisx > 115 and sourisx < 215 and sourisy > 255 and sourisy < 355 and PositionClic == 4:
+        #
+        # Rétablit l'apparence basique du bouton
+        # 
+        PositionClic = 0
+        logo.f_BoutonArriere()
         #
         # Sinon, si la souris n'est sur aucun des boutons:
         #
@@ -86,7 +132,12 @@ def f_RecupCoord():
         logo.f_BoutonAvant()
         logo.f_BoutonDroite()
         logo.f_BoutonArriere()
-    
+        PositionClic = 0
+
+        ######################################
+        # FIN DE LA PARTIE NON FONCTIONNELLE.#
+        ######################################
+
     #
     # Associe le clic gauche de la souris à la fonction ci-dessus
     #
