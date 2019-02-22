aaa4b1881c836e097cb1c50dafdb4bb7ed7aecf3
Salvatore <romeo83var@gmail.com>
2019-02-22 21:05:35 +0100

    fonction de gerstion d'erreur

--- gestion-erreur.py
+++ gestion-erreur.py
@@ -11,4 +11,21 @@
 
 def f_message_erreur(para_message_erreur):
 
+	liste = []
+	compt = 0
+
+	while compt < len(para_message_erreur):
+		liste.append("!")
+		compt += 1
+
+	cara_erreur = ""
+
+	for e in liste:
+		cara_erreur = cara_erreur + e
+
+	print(cara_erreur)
 	print(para_message_erreur)
+	print(cara_erreur)
+
+
+f_message_erreur("Erreur lors de l'ouverture du fichier")
