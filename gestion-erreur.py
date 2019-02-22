e1d43614a6f89736d82e09090863cc6f5a9a30b4
Salvatore <romeo83var@gmail.com>
2019-02-22 20:56:39 +0100

    TTTTTTTTTTT

--- gestion-erreur.py
+++ gestion-erreur.py
@@ -7,6 +7,8 @@
 # Message predefinie, ne continent qu'une fonction, afficher les erreurs
 #
 #
-#
-#
 ###########################################################################################################################
+
+def f_message_erreur(para_message_erreur):
+
+	print(para_message_erreur)
