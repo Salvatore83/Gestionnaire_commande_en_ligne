243331e20314d72e1b389c2fe1ef9a302d593438
Salvatore <romeo83var@gmail.com>
2019-05-15 11:50:17 +0200

    modif

--- robot/commandes_robot.py
+++ robot/commandes_robot.py
@@ -24,8 +24,8 @@ def f_initialisation_capteurs():
         GP.setup(ledPin, GP.OUT)
         return 'Succes'
     except:
-        # changer en erreur lors du fonctionnement du programme. 
-        # comme je ne code pas avec le robot, obligé de faire ça 
+        # changer en erreur lors du fonctionnement du programme.
+        # comme je ne code pas avec le robot, obligé de faire ça
         # Pour les tests, ce sera en 'Succes'
         return 'Erreur'
 
@@ -42,8 +42,53 @@ def f_eteindre_LED(para_LED):
     print("LA LED", para_LED, "est éteinte")
 
 # Fais actionne le moteur des roues du robot
-def f_robot_moteur_roues(para_roue_gauche, para_roue_droite, para_pourcentage_gauche, para_pourcentage_droite):
-    pass
+def f_robot_moteur_roues():
+
+    GPIO.setmode(GPIO.BOARD)   ##je prefere la numerotation BOARD plutot que BCM
+
+    Moteur1A = 16      ## premiere sortie du premier moteur, pin 16
+    Moteur1B = 18      ## deuxieme sortie de premier moteur, pin 18
+    Moteur1E = 22      ## enable du premier moteur, pin 22
+
+    GPIO.setup(Moteur1A,GPIO.OUT)  ## ces trois pins du Raspberry Pi sont des sorties
+    GPIO.setup(Moteur1B,GPIO.OUT)
+    GPIO.setup(Moteur1E,GPIO.OUT)
+
+    pwm = GPIO.PWM(Moteur1E,50)   ## pwm de la pin 22 a une frequence de 50 Hz
+    pwm.start(100)   ## on commemnce avec un rapport cyclique de 100%
+
+
+    print ("Rotation sens direct, vitesse maximale (rapport cyclique 100%)")
+    GPIO.output(Moteur1A,GPIO.HIGH)
+    GPIO.output(Moteur1B,GPIO.LOW)
+    GPIO.output(Moteur1E,GPIO.HIGH)
+
+    sleep(5)  ## on laisse tourner le moteur 5 secondes avec des parametres
+
+    pwm.ChangeDutyCycle(20)  ## modification du rapport cyclique a 20%
+
+    print ("Rotation sens direct, au ralenti (rapport cyclique 20%)")
+
+    time.sleep(5)
+
+    print ("Rotation sens inverse, au ralenti (rapport cyclique 20%)")
+    GPIO.output(Moteur1A,GPIO.LOW)
+    GPIO.output(Moteur1B,GPIO.HIGH)
+
+    time.sleep(5)
+
+    pwm.ChangeDutyCycle(100)
+    print("Rotation sens inverse, vitesse maximale (rapport cyclique 100%)")
+    time.sleep(5)
+
+
+    print ("Arret du moteur")
+    GPIO.output(Moteur1E,GPIO.LOW)
+
+    pwm.stop()    ## interruption du pwm
+
+    GPIO.cleanup()
+
 
 def f_calculer_distance(para_US):
     pass
